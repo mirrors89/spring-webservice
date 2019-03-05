165497258f8a29322eb6c88d126cafbecb75eba2
mirrors89 <lee01494@gmail.com>
2019-03-05 21:09:59 +0900

    init project

---
+++ src/main/java/com/mirrors89/webservice/Application.java
@@ -0,0 +1,13 @@
+package com.mirrors89.webservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
