2350d8e25ea97fa8efee8f57ae1935ccf1dcfc7c
mirrors89 <lee01494@gmail.com>
2019-03-07 20:33:30 +0900

    add post list

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 # spring-webservice
+
+[![Build Status](https://travis-ci.com/cheese10yun/spring-SOLID.svg?branch=master)](https://travis-ci.com/cheese10yun/spring-SOLID)
+[![Coverage Status](https://coveralls.io/repos/github/cheese10yun/spring-SOLID/badge.svg?branch=master)](https://coveralls.io/github/cheese10yun/spring-SOLID?branch=master)
+[![codecov](https://codecov.io/gh/cheese10yun/spring-SOLID/branch/master/graph/badge.svg)](https://codecov.io/gh/cheese10yun/spring-SOLID)
+
+
 스프링부트로 구축하는 웹 서비스
 
 참고자료
--- src/main/java/com/mirrors89/webservice/dto/posts/PostsMainResponseDto.java
+++ src/main/java/com/mirrors89/webservice/dto/posts/PostsMainResponseDto.java
@@ -1,4 +1,31 @@
 package com.mirrors89.webservice.dto.posts;
 
+import com.mirrors89.webservice.domain.posts.Posts;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Optional;
+
+@Getter
 public class PostsMainResponseDto {
+
+    private Long id;
+    private String title;
+    private String author;
+    private String modifiedDate;
+
+    public PostsMainResponseDto(Posts entity) {
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.author = entity.getAuthor();
+        modifiedDate = toStringDateTime(entity.getModifiedDate());
+    }
+
+    private String toStringDateTime(LocalDateTime localDateTime) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        return Optional.ofNullable(localDateTime)
+                .map(formatter::format)
+                .orElse("");
+    }
 }
